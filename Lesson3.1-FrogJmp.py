f2f79511bdca43a345bc3aaf3a729c61d6094149
alejandrogarza <alejandrogarza@users.noreply.github.com>
2019-11-08 21:29:12 +0100

    Create Lesson3.1-FrogJmp.py

---
+++ Lesson3.1-FrogJmp.py
@@ -0,0 +1,7 @@
+# you can write to stdout for debugging purposes, e.g.
+# print("this is a debug message")
+
+def solution(X, Y, D):
+    # write your code in Python 3.6
+    import math
+    return math.ceil((Y-X)/D)
